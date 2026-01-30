ac01b88c2254bc18ea6217026fd9a2ae79d6e1f6
jankeladoktor <vrabecjan321@gmail.com>
2026-01-30 14:02:57 +0100

    animacije
    daj ti Bog zdravlja

--- gamejamefoi/Assets/Skripte/Igrac.cs
+++ gamejamefoi/Assets/Skripte/Igrac.cs
@@ -8,12 +8,14 @@ public class Igrac : MonoBehaviour
     private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         
     }
 
@@ -24,8 +26,17 @@ public class Igrac : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
-        
+        animator.SetBool("isWalking", true);
+        if (context.canceled) {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+
+        }
+
         moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
         
     }
    
